a1627eff5a5153493e5ffe277ee90920ba0c358a
Mattia Del Papa Keyfor <mattia.delpapa@keyfor.it>
2019-01-21 16:20:41 +0100

    Creata e aggiunta chiama alla api_esterna

--- api_esterna.php
+++ api_esterna.php
@@ -1,42 +1,45 @@
 <?php
-/* API per la stampa di tutti i tassi supeririori */
-require 'config.php';    //includiamo file di configurazione
-header("Content-Type: application/json; charset=UTF-8");   /* info passate tramite header per indicare la tipologia di valore
-                                                              ritornato in seguito all'elaborazione del codice della pagina web */
-$connection = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);     //connessione al db
-if (!$connection) {      //se la connessione non è avvenuta stampiamo un messaggio di avvertimento
-  echo "Errore: Impossibile connettersi al database MySQL." . PHP_EOL;
-  echo "<br />Debugging errno: " . my_sqli_errno() . PHP_EOL;
-  echo "<br />Debugging error: " . my_sqli_error() . PHP_EOL;
-  exit;    //terminiamo l'esecuzione dello script
-}
-/*
-$sql = "SELECT * FROM `Tasso Superiore`";    //query che andremo ad eseguire
-$final_array = array();    //creiamo array vuoto
-if (mysqli_real_query($connection, $sql)) {                  //tramite questa funz. eseguiamo la query memorizz. nella variabile $sql
-  if ($result = mysqli_use_result($connection)) {              //tramite questa funzione preleviamo l'ultimo risultato (della query) eseguito sul database $connection
-    while ($rows = mysqli_fetch_row($result)) {           //tramite questa funzione analizziamo tutte le righe (una dopo l'altra) partendo dalla 1° fino all'ultima, fermandoci appena viene restituito 'false'
-      $final_array[] = array(
-                  "Anno" => "$rows[0]",
-                  "Regione" => "$rows[1]",
-                  "Tasso di scolarizzazione" => "$rows[2]",    //memorizziamo nell'array le info che ci interessano
+/* API per la stampa delle università italiane e dei loro siti web */
+//includiamo file di configurazione
+require 'config.php';
+//impostiamo informazioni header richiesta HTTP
+header("Content-Type: text/plain; charset=UTF-8");
+//inizializzazione della richiesta HTTP tramite CURL
+$url = 'http://universities.hipolabs.com/search?country=italy';
+$handle = curl_init($url);
 
-                );
-    }
-    //se l'array risulta essere vuoto
+//richiesta della risposta HTTP come stringa
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+//esecuzione della richiesta HTTP
+$response = curl_exec($handle);
+//estrazione del codice di risposta (HTTP status)
+$http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+
+if($http_code == 200) {
+   $final_array = array();    //creiamo array vuoto
+    //risposta HTTP ok
+    $conta = 0;
+    $data = json_decode($response, TRUE);
+    foreach ($data as $rows){
+                $final_array[] = array(
+                    "Nome Universita" => $data[$conta]['name'],    //memorizziamo nell'array le info che ci interessano
+                    "Pagina Web" => $data[$conta]['web_pages'],
+                    "Dominio" => $data[$conta]['domains']
+                    );
+                    $conta += 1;
+        }
     if (count($final_array) == 0) {
-      http_response_code(400);        //modifichiamo il codice di risposta di HTTP impostandolo 400
-      exit;                           //terminiamo l'esecuzione dello script
-    }
-  }
+        http_response_code(400);        //modifichiamo il codice di risposta di HTTP impostandolo 400
+        exit;                           //terminiamo l'esecuzione dello script
+        }
 } else {
+  //risposta HTTP con errore
   http_response_code(400);        //modifichiamo il codice di risposta di HTTP impostandolo 400
   exit;                           //terminiamo l'esecuzione dello script
 }
-$elencoSupJson = json_encode($final_array);       //codifichiamo l'array in json per trasferimento dati tramite richiesta HTTP
-mysqli_free_result($result);    //questa funzione serve per indicare che il risultato della query non ci serve più e liberare la memoria
-mysqli_close($connection);            //questa funzione termina la connessione col db
-echo "$elencoSupJson";
-*/
+$elencoUnivItaliaJson = json_encode($final_array, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);       //codifichiamo l'array in json per trasferimento dati tramite richiesta HTTP
+echo "$elencoUnivItaliaJson";
+
 exit;
+
 ?>
--- functions.php
+++ functions.php
@@ -90,14 +90,57 @@ function stampa_tassi($http_code, $response) {
 }    //end function
   
 function api_esterna($http_code, $response){
+  //memorizziamo l'url a cui effettuare la richiesta HTTP
   if ($http_code == 200) {
-      $data = json_decode($response, true);
-      echo "\n\n----------------------\n";
-      echo "| API ESTERNA |";
-      echo "\n----------------------\n";
-      //foreach ($data as $info) {
-
-      //}
+    //risposta HTTP ok
+    $data = json_decode($response, true);
+    //stampa info tassi universitari
+    echo "\n\n-------------------------------------------------------------------------------------------------------------------------------------------------\n";
+    echo "|\t\t\t Nome Università \t\t\t\t|\t Pagina Web \t\t\t|\t Dominio \t\t|";
+    echo "\n-------------------------------------------------------------------------------------------------------------------------------------------------";
+    foreach ($data as $info) {
+      //nome università tabbato bene per lunghezza
+        printf("\n");
+        if (strlen($info['Nome Universita'])<= 22 ) {
+          printf(" %s\t\t\t\t\t\t\t|", $info['Nome Universita']);
+        }else if (strlen($info['Nome Universita'])<= 30 ) {
+          printf(" %s\t\t\t\t\t\t|", $info['Nome Universita']);
+        }else if (strlen($info['Nome Universita'])<= 37 ) {
+          printf(" %s\t\t\t\t\t|", $info['Nome Universita']);
+        }else if (strlen($info['Nome Universita'])<= 45 ) {
+          printf(" %s\t\t\t\t|", $info['Nome Universita']);
+        }else if (strlen($info['Nome Universita'])< 55 ) {
+          printf(" %s\t\t\t|", $info['Nome Universita']);
+        }else if (strlen($info['Nome Universita'])<= 60 ) {
+          printf(" %s\t\t|", $info['Nome Universita']);
+        }else if (strlen($info['Nome Universita'])<= 65 ) {
+          printf(" %s\t|", $info['Nome Universita']);
+        }
+        //pagina web 
+        if (strlen(implode(" ",$info['Pagina Web']))<= 8 ) {
+          printf("  %s\t\t\t\t|", implode(" ",$info['Pagina Web']));
+        } else if (strlen(implode(" ",$info['Pagina Web']))< 21 ) {
+          printf("  %s\t\t\t|", implode(" ",$info['Pagina Web']));
+        } else if (strlen(implode(" ",$info['Pagina Web']))<= 28 ) {
+          printf("  %s\t\t|", implode(" ",$info['Pagina Web']));
+        } else {//if (strlen(implode(" ",$info['Pagina Web']))<= 30 ) {
+          printf("  %s\t|", implode(" ",$info['Pagina Web']));
+        }
+        //dominio
+        if (strlen(implode(" ",$info['Dominio'])) < 6 ) {
+          printf("  %s\t\t\t\t|", implode(" ",$info['Dominio']));
+        } else if (strlen(implode(" ",$info['Dominio']))< 13 ) {
+          printf("  %s\t\t\t|", implode(" ",$info['Dominio']));
+        } else if (strlen(implode(" ",$info['Dominio']))<= 18 ) {
+          printf("  %s\t\t|", implode(" ",$info['Dominio']));
+        } else {//if (strlen(implode(" ",$info['Dominio']))<= 30 ) {
+          printf("  %s\t|", implode(" ",$info['Dominio']));
+        }
+    }
+    echo "\n-------------------------------------------------------------------------------------------------------------------------------------------------\n";
+  } else {
+    //se ritorna un codice di errore dalla richiesta HTTP
+    echo "\nATTENZIONE FUNCTION ---> La richiesta HTTP ha restituito il codice d'errore #{$http_code}." . PHP_EOL;
   }
-}
+}    //end function
 ?>
